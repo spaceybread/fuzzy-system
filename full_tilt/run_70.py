5d626eb52c2ca2eeb7d785738ca4f5ed70d4b6bb
spaceybread <spaceybread@gmail.com>
2025-10-30 16:36:46 -0500

    TMR 70 numbers

---
+++ full_tilt/run_70.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import subprocess
+import sys
+import os
+
+data_dir = sys.argv[1]
+
+lat_ds = data_dir + "/lat_ds.npy"
+rad_ds = data_dir + "/radial_ds.npy"
+
+results_folder = data_dir + "/results"
+
+try: os.mkdir(results_folder)
+except FileExistsError: pass
+
+venv_python = Path("~/Desktop/.venv/bin/python3").expanduser()
+
+# gauss 1 stack classifier
+script_path = "optimizers/run_gauss_1_stack.py"
+args = [lat_ds, results_folder + "/gauss_1_stack_classifier_70.csv"]
+
+cmd = [str(venv_python), str(script_path)] + args
+result = subprocess.run(cmd)
+
+# gauss 2 stack classifier
+script_path = "optimizers/run_gauss_2_stack.py"
+args = [lat_ds, results_folder + "/gauss_2_stack_classifier_70.csv"]
+
+cmd = [str(venv_python), str(script_path)] + args
+result = subprocess.run(cmd)
